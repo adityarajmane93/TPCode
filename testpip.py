553f39eb192e2ab9c58f651d276dbfbdeee70bb0
Aditya Rajmane <anr331@nyu.edu>
2016-10-05 09:53:11 -0400

    get all the package info in python
    gets all the names and versions of packages which can be imported.

---
+++ testpip.py
@@ -0,0 +1,6 @@
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+	for i in installed_packages])
+for it in installed_packages_list:
+	print it+"\n"
